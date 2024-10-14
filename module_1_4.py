9c5f943816d05bec766117e46d03191fa89eb55d
Pashabrrrr <p_bazarov@mail.ru>
2024-10-15 01:35:27 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string = input("Введите свое имя:")
+print("Количество символов в строке:", len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+N = len(my_string)
+print(my_string[N-1])
+
